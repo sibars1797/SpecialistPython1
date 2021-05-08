7a6463d121e25f3359350a464f7ab423a7b7f170
sibars1797 <83125730+sibars1797@users.noreply.github.com>
2021-05-08 14:04:33 +0700

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -3,8 +3,23 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 
 def lucky_ticket(ticket_number):
-    # TODO: your code here
-    pass
+    parts = list(divmod(ticket_number, 1000))
+    for i in range(2):
+        s = 0
+        while parts[i] > 0:
+            s += parts[i] % 10
+            parts[i] //= 10
+        parts[i] = s
+    if parts[0] == parts[1]:
+        return 'YES'
+    else:
+        return 'NO'
+    
+def summa(*args):
+    s = 0
+    for arg in (args):
+        s += arg
+    return s
 
 
 # Тестируем функцию
