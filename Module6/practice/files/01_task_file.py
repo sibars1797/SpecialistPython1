14371d00011ee2efc13d5defd751503deac57348
sibars1797 <83125730+sibars1797@users.noreply.github.com>
2021-05-08 17:55:37 +0700

    Update 01_task_file.py

--- Module6/practice/files/01_task_file.py
+++ Module6/practice/files/01_task_file.py
@@ -4,3 +4,16 @@
 # 2. Узнайте количество непробельный символов в данном файле
 # 3. Узнайте количество стихотворений, если известно,
 # что каждое стихотворение отделяется пустой строкой от предыдущего
+f = open('data/limericks.txt', 'r', encoding='utf-8')
+simbols = 0
+poem_count = 0
+for line in f:
+    for s in line:
+        if not s.isspace():
+            simbols += 1
+        elif line[0] == '\n':
+            poem_count += 1
+            simbols += 1
+print()
+print(f'Непробельных символов: {simbols}')
+print(f'Стихотворений: {poem_count + 1}')
