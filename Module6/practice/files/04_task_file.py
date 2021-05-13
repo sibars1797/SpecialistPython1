f220a5bde75c24c408b5981b9c045f16cf54d438
sibars1797 <83125730+sibars1797@users.noreply.github.com>
2021-05-14 00:16:36 +0700

    Update 04_task_file.py

--- Module6/practice/files/04_task_file.py
+++ Module6/practice/files/04_task_file.py
@@ -9,3 +9,17 @@
 # Подсказка:
 # Чтобы получить список больших букв русского алфавита:
 # print(list(map(chr, range(ord('А'), ord('Я')+1))))
+
+first_symb_fruits = {}                                                    # формирование словаря с ключами от "А" до "Я"
+for symb in list(map(chr, range(ord('А'), ord('Я') + 1))):                #
+    first_symb_fruits.update([(symb, [])])                                #
+#first_symb_fruits = dict.fromkeys(list(map(chr, range(ord('А'), ord('Я') + 1))), [])   #при таком способе создания словаря метод append работает неожиданно, надо разбираться
+with open('data/fruits.txt', encoding='utf-8') as f:
+    for line in f:
+        line = line.strip()
+        if line:
+#            first_symb.setdefault(line[0], [])   #если не обязательно делать файл с каждой буквой, данный метод позволяет обойтись без предаварительного создания словаря
+            first_symb_fruits[line[0]].append(line + '\n' + '\n')
+for symb in first_symb_fruits:
+    with open(f'data/fruits/fruits_{symb}.txt', 'w', encoding='utf-8') as f:
+        print(*(first_symb_fruits[symb]), file=f)
